5540de74d365a908c7b6c706ba416100d7e4f0c3
Thomas Adamcik <thomas@adamcik.no>
2010-05-10 21:48:17 +0200

    Update spotify handler to ignore open.spotify.com urls as urlpaser will handle them

--- catsitter/plugins/spotify.py
+++ catsitter/plugins/spotify.py
@@ -8,38 +8,33 @@ from catsitter import register, decode
 
 SPOTIFY_URL = 'http://ws.spotify.com/lookup/1/?uri=spotify:%s:%s'
 
-@register('(?P<spotifyline>.*(spotify:(track|artist|album):[A-Za-z0-9]+|open.spotify.com/(track|artist|album)/[A-Za-z0-9]+).*)')
+@register('(?P<spotifyline>.*spotify:(track|artist|album):[A-Za-z0-9]+.*)')
 def handler(spotifyline=None):
     result = []
 
     for type, id in re.findall('spotify:(track|artist|album):([A-Za-z0-9]+)', spotifyline):
-        fetch(type, id, result)
-    for type, id in re.findall('open.spotify.com/(track|artist|album)/([A-Za-z0-9]+)', spotifyline):
-        fetch(type, id, result)
+        try:
+            xml = urllib.urlopen(SPOTIFY_URL % (type, id)).read()
+        except IOError, e:
+            continue
+
+        if not xml:
+            continue
+
+        data  = fromstring(xml)
+
+        if type == 'artist':
+            result.append(u'>> %s' % data.name)
+        else:
+            artists = [unicode(artist.name) for artist in data.artist]
+
+            if len(artists) > 4:
+                artists = u', '.join(artists[:4]) + u', ...'
+            elif len(artists) > 1:
+                artists = u', '.join(artists[:-1]) + u' and ' + artists[-1]
+            else:
+                artists = artists[0]
+
+            result.append(u'>> %s by %s' % (data.name, artists))
 
     return result
-
-def fetch(type, id, result):
-    try:
-        xml = urllib.urlopen(SPOTIFY_URL % (type, id)).read()
-    except IOError, e:
-        return []
-
-    if not xml:
-        return
-
-    data  = fromstring(xml)
-
-    if type == 'artist':
-        result.append(u'%s' % data.name)
-    else:
-        artists = [unicode(artist.name) for artist in data.artist]
-
-        if len(artists) > 4:
-            artists = u', '.join(artists[:4]) + u', ...'
-        elif len(artists) > 1:
-            artists = u', '.join(artists[:-1]) + u' and ' + artists[-1]
-        else:
-            artists = artists[0]
-
-        result.append(u'%s by %s' % (data.name, artists))
