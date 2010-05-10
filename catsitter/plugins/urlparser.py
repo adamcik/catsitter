4f08bf3cf38dae7da4100293f4dc883a4fc7e94b
Thomas Adamcik <thomas@adamcik.no>
2010-05-10 21:04:44 +0200

    Refine urlparser regexp and handling

--- catsitter/plugins/urlparser.py
+++ catsitter/plugins/urlparser.py
@@ -10,36 +10,29 @@ from lxml.html import fromstring
 from catsitter import decode, register
 
 RE_URL = re.compile(r'(?:\b|[(])(?P<scheme>[a-z+]+://)?'
-                    r'(?P<hostpath>(?:[^ ]+\.)+[^/]+(?:/[^?#]*)?)'
+                    r'(?P<host>(?:[^ #?://]+\.)+[^ #?://]+)'
+                    r'(?P<path>/[^?#]*)?'
                     r'(?P<query>\?[^#]*)?(?:\b|[,.?!])', re.UNICODE)
 
 
 def parse_url(line):
-    for scheme, hostpath, query in RE_URL.findall(line):
+    for scheme, host, path, query in RE_URL.findall(line):
         if scheme and not re.match('https?://', scheme):
             continue
 
-        try:
-            host, path = hostpath.split('/', 1)
-        except ValueError:
-            host, path = hostpath, ''
         scheme = scheme.encode('utf-8') or 'http://'
         host = host.encode('idna')
         path = path.encode('utf-8') or ''
         query = query.encode('utf-8') or ''
-        yield ''.join([scheme, host, '/', path, query])
+        yield ''.join([scheme, host, path, query])
 
 def pretty_url(line):
-    for scheme, hostpath, query in RE_URL.findall(line):
-        try:
-            host, path = hostpath.split('/', 1)
-        except ValueError:
-            host, path = hostpath, ''
+    for scheme, host, path, query in RE_URL.findall(line):
         scheme = scheme or 'http://'
         host = host.decode('idna')
         path = decode(urllib.unquote(path)) or ''
         query = decode(urllib.unquote(query)) or ''
-        return u''.join([scheme, host, u'/', path, query])
+        return u''.join([scheme, host, path, query])
 
 @register('(?P<urlline>[^!].*\w+\.\w+.*)')
 def handler(urlline=None):
