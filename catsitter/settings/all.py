23211d956e5b309f0a6de6aa346296b5da568113
Thomas Adamcik <thomas@adamcik.no>
2011-09-11 14:06:01 +0200

    Reduce plugins in all/default settings

--- catsitter/settings/all.py
+++ catsitter/settings/all.py
@@ -1,7 +1,7 @@
 import catsitter.plugins.busstuc
-import catsitter.plugins.callsign
-import catsitter.plugins.frequency
-import catsitter.plugins.launchpad
+#import catsitter.plugins.callsign
+#import catsitter.plugins.frequency
+#import catsitter.plugins.launchpad
 import catsitter.plugins.middag
 import catsitter.plugins.urlparser
 import catsitter.plugins.spotify
